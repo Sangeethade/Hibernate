365e024d396903412d63f08a4d84fb929afd93d3
Sangeethade <102943538+Sangeethade@users.noreply.github.com>
2022-07-10 21:11:05 +0530

    Add files via upload

---
+++ lab activity/com/com.Entity/Account.java
@@ -0,0 +1,38 @@
+package com.Entity;
+    @Table(name="account_details")
+	@Entity
+	public class Account {
+	@Id
+	@Column(length=10,unique=true)
+	private long accountNo;
+	@Column(length=20)
+	private String accountHolderName;
+	private double accBalance;
+	@Column(length=20)
+	private String accType;
+	public long getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(long accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getAccountHolderName() {
+		return accountHolderName;
+	}
+	public void setAccountHolderName(String accountHolderName) {
+		this.accountHolderName = accountHolderName;
+	}
+	public double getAccBalance() {
+		return accBalance;
+	}
+	public void setAccBalance(double accBalance) {
+		this.accBalance = accBalance;
+	}
+	public String getAccType() {
+		return accType;
+	}
+	public void setAccType(String accType) {
+		this.accType = accType;
+	}
+	}
+
